8c0123871e46492bbac3ba66dbdeac4009306990
Ulf Bengtsson <Ulf.Bengtsson.Lexicon@outlook.com>
2021-05-20 12:18:10 +0200

    more role limit controller

--- WebAppCarShop/Controllers/SalesController.cs
+++ WebAppCarShop/Controllers/SalesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ using WebAppCarShop.Models.ViewModel;
 
 namespace WebAppCarShop.Controllers
 {
+    [Authorize(Roles = "Admin, SalesEmployee")]
     public class SalesController : Controller
     {
         private readonly ISaleService _saleService;
