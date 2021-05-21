050b5d7ac47b3ee4340a283cb45b7c2244179771
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2021-05-21 10:18:32 +0200

    added seeding

---
+++ WebAppCarShop/Database/DbInitializer.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCarShop.Database
+{
+    public class DbInitializer
+    {
+        public static void Initialize(
+            CarShopDbContext context, 
+            RoleManager<IdentityRole> roleManager, 
+            UserManager<IdentityUser> userManager
+            )
+        {
+            //context.Database.EnsureCreated();//If not using EF migrations
+            context.Database.Migrate();
+
+            if (context.Roles.Any())
+            {
+                return; //i´ll assume database is seeded becouse i found roles
+            }
+
+            //------ Seed into database -----------------------------------------------------------
+
+            string[] rolesToSeed = new string[] { "Admin", "SalesEmployee", "Client" };
+
+            foreach (var roleName in rolesToSeed)
+            {
+                IdentityRole role = new IdentityRole(roleName);
+
+                var result = roleManager.CreateAsync(role).Result;
+
+                if (!result.Succeeded)
+                {
+                    throw new Exception("Faild to create Role: " + roleName);
+                }
+            }
+
+            IdentityUser user = new IdentityUser()
+            {
+                UserName = "AdminPower",
+                Email = "a@a.a",
+                PhoneNumber = "123123123"
+            };
+            IdentityResult resultUser = userManager.CreateAsync(user, "Qwerty!23456").Result;
+
+            if (!resultUser.Succeeded)
+            {
+                throw new Exception("Faild to create Admin Acc: AdminPower");
+            }
+
+            IdentityResult resultAssign = userManager.AddToRoleAsync(user, rolesToSeed[0]).Result;
+
+            if (!resultAssign.Succeeded)
+            {
+                throw new Exception($"Faild to grant {rolesToSeed[0]} role to AdminPower");
+            }
+        }
+    }
+}
